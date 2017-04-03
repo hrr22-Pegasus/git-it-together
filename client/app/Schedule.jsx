9376cb80b504d44e86c8572caa8722f77d4cb78e
Samy Kebaish <skebaish1992@gmail.com>
2017-04-03 14:00:12 -0700

    Schedule is color responsive based on task

--- client/app/Schedule.jsx
+++ client/app/Schedule.jsx
@@ -27,6 +27,61 @@ class Schedule extends React.Component {
         this.getDeliverables();
     }
 
+
+    eventStyleGetter(event, start, end, isSelected) {
+      console.log("HERE IS THE EVENT", event.status);
+      var today = moment().add(10, 'days').calendar();
+
+      var eventStatus = event.status;
+      var eventEndDate = event.end.getDate();
+      var eventEndMonth = event.end.getMonth();
+      var splitToday = today.toString().split('/');
+      var todayMonth = splitToday[0]-1;
+      var todayDate = splitToday[1];
+
+
+      var custombackgroundColor = '#a641f4';
+
+      if (eventStatus === "current") {
+        custombackgroundColor = '#f4eb42';
+        if (eventEndDate<todayDate && eventEndMonth<=todayMonth) {
+          console.log("LATE");
+          custombackgroundColor = '#cc0000';
+        }
+      }
+
+      if (eventStatus === "icebox") {
+        custombackgroundColor = '#00ffff';
+      }
+
+      if (eventStatus === "complete") {
+        custombackgroundColor = '#1ca428';
+      }
+
+      if (eventStatus === "backlog") {
+        custombackgroundColor = '#ffa500';
+      }
+
+      var style = {
+        backgroundColor: custombackgroundColor,
+        borderRadius: '0px',
+        opacity: 0.8,
+        color: 'black',
+        border: '0px',
+        display: 'block'
+    };
+    return {
+        style: style
+     };
+    }
+
+
+
+
+
+
+
+
     getDeliverables(cb) {
     var context = this;
     var project = this.state.project;
@@ -45,15 +100,8 @@ class Schedule extends React.Component {
   }]
     var events = [];
       response.data.forEach(function(deliverable) {
-        if (deliverable.status === 'complete') {
-          deliverables.complete.push(deliverable);
-        } else if (deliverable.status === 'backlog') {
-          deliverables.backlog.push(deliverable);
-        } else if (deliverable.status === 'icebox') {
-          deliverables.icebox.push(deliverable);
-        } else if (deliverable.status === 'current') {
-          deliverables.current.push(deliverable);
-          var title = deliverable.task + "-" + deliverable.owner;
+
+        var title = deliverable.task + "-" + deliverable.owner;
           var year = parseInt(deliverable.startDate.substring(0,4));
           var month = parseInt(deliverable.startDate.substring(5,7));
           var day = parseInt(deliverable.startDate.substring(8,10));
@@ -63,8 +111,17 @@ class Schedule extends React.Component {
           var eventToPush = {
             'title': title,
             'start': new Date(year, month-1, day),
-            'end': new Date(year2, month2-1, day2)
+            'end': new Date(year2, month2-1, day2),
+            'status': deliverable.status
           };
+        if (deliverable.status === 'complete') {
+          deliverables.complete.push(deliverable);
+        } else if (deliverable.status === 'backlog') {
+          deliverables.backlog.push(deliverable);
+        } else if (deliverable.status === 'icebox') {
+          deliverables.icebox.push(deliverable);
+        } else if (deliverable.status === 'current') {
+          deliverables.current.push(deliverable);
         }
           deliverables.dates.push(eventToPush);
           // console.log(eventToPush);
@@ -78,6 +135,7 @@ class Schedule extends React.Component {
       <BigCalendar
         {...this.props}
         events={this.state.deliverables.dates}
+        eventPropGetter={(this.eventStyleGetter)}
         // defaultDate={new Date(2017, 3, 31)}
       />
     )
